49795d7bca2a8288d4acd84dd1502d217211a6d1
Wong Kai Lin <kwon0061@student.monash.edu>
2021-07-29 15:56:00 +0800

    test

---
+++ test.py
@@ -0,0 +1,23 @@
+##def parsexVowel(strval):
+##    vowels="aeiouAEIOU"
+##    result=""
+##    for char in strval:
+##        if char not in vowels:
+##            result=result+char
+##    return result
+##print(parsexVowel("Your time is limited."))
+##print(parsexVowel("So don't waste it living someone else's life."))
+##print(parsexVowel("Don't be trapped by dogma."))
+
+
+x = 1
+y = 2
+
+# x = 2 y =1
+
+##temp = y # 2
+##y = x # 1
+##x = temp # 2
+
+x,y = y,x
+print(x,y)
